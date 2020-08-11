1f1aa1040c9d2264e510a9790a2586c02847a92b
mabi08 <50304545+mabi08@users.noreply.github.com>
2020-08-11 17:15:11 +0200

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -8,7 +8,7 @@ from airflow.operators.python_operator import PythonOperator
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime.utcnow(),
+    "start_date": datetime(2015, 6, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
