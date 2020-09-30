15be4945e78340a1aa3af5a865fa731d43c4cdf3
uia81723 <maik.bialas@conti.de>
2020-09-30 12:07:47 +0200

    no imports

--- titanic.py
+++ titanic.py
@@ -1,19 +1,6 @@
 import boto3
 import numpy as np 
 import pandas as pd 
-import seaborn as sns
-from matplotlib import pyplot as plt
-from matplotlib import style
-from sklearn import linear_model
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC, LinearSVC
-from sklearn.naive_bayes import GaussianNB
-
 
 from datetime import datetime, timedelta
 from airflow import DAG
