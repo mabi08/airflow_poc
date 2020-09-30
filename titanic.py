21b8a169255681eea0129f2299746e43b0fcfe6e
uia81723 <maik.bialas@conti.de>
2020-09-30 12:22:06 +0200

    -

--- titanic.py
+++ titanic.py
@@ -65,7 +65,7 @@ t1 = KubernetesPodOperator(
     image="mabi/titanic-airflow:latest",
     task_id='read_s3_data',
     namespace="airflow-ng",
-    name="read_s3_data",
+    name="read-s3-data",
     dag=dag)
 
 # t2 = PythonOperator(
